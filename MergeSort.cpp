362e93ffa67ab4b2d96768f2b0839ef5d9d3a9cb
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-04-02 19:12:32 +0700

    Menyelesaikan proses merging

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,30 +1,19 @@
-void mergeSort(int low, int high)
-{
-    if (low >= high)
+    while (j <= high)
     {
-        return;
-    }
-
-    int mid = (low + high) / 2;
-
-    mergeSort(low, mid);
-    mergeSort(mid + 1, high);
-
-    int i = low;
-    int j = mid + 1;
-    int k = low;
-
-    while (i <= mid && j <= high)
-    {
-        if (arr[i] <= arr[j])
-        {
-            B[k] = arr[i];
-            i++;
-        }
-        else
-        {
-            B[k] = arr[j];
-            j++;
-        }
+        B[k] = arr[j];
+        j++;
         k++;
     }
+
+    while (i <= mid)
+    {
+        B[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
+}
